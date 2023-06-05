5f684b4a87705ee94bf2c405757ce744aa257d9f
Laura Solorio <laurinzqui@gmail.com>
2023-06-04 22:43:07 -0600

    se agrega nuevo ejercicio y notas sobre funciones JavaScript

---
+++ ejercicioFunciones.js
@@ -0,0 +1,19 @@
+
+function calculaVelocidad(distancia, tiempo) {
+    var distancia = 100;
+    var tiempo = 15;
+   velocidad = distancia/tiempo;
+   console.log( velocidad);
+}
+calculaVelocidad();
+
+// funciones anonimas
+let funcionAnonima = function (a, b) {
+
+}
+
+
+//funcion para dar unn saludo personalizado
+function saludo (nombre){
+    console.log("hola" + nombre + "que bueno que usas nuestra aplicacion")
+}
